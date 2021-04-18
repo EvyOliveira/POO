fb0477fcfee6d168c86a674cbb6d3190193441a2
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-04-17 22:57:47 -0300

    Refactor declares variables to customers

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const cliente2CPF = 88822233309;
 const cliente2Agencia = 1001;
 const cliente2Saldo = 0;
 
-const cliente2Nome = "Gustavo";
-const cliente2CPF = 44488855598;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente3Nome = "Gustavo";
+const cliente3CPF = 44488855598;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1Nome);
+console.log(cliente2Nome);
+console.log(cliente3Nome);
