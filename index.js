b6021904eecf815a2bda226442d825d11302bbd9
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-04-17 23:17:32 -0300

    Refactor model class for variables associated with an object

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const cliente1Nome = "Evelyn";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+const cliente3 = new Cliente();
 
-const cliente3Nome = "Gustavo";
-const cliente3CPF = 44488855598;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+cliente1.nome = "Evelyn";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
 
-console.log(cliente1Nome);
-console.log(cliente2Nome);
-console.log(cliente3Nome);
+cliente2.nome = "Alice";
+cliente2.cpf = 44488855598;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+cliente3.nome = "Gustavo";
+cliente3.cpf = 44488855598;
+cliente3.agencia = 1001;
+cliente3.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
