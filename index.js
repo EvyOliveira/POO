4f51c9655c7988c38b64ec3065ad95d26e457ec0
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-04-20 17:19:30 -0300

    Feat getters and setters in the class ContaCorrente * Refactor client private attribute in the class ContaCorrente

--- index.js
+++ index.js
@@ -5,24 +5,19 @@ const cliente1 = new Cliente();
 cliente1.nome = "Evelyn";
 cliente1.cpf = 11122233309;
 
-// const cliente2 = new Cliente();
-// cliente2.nome = "Alice";
-// cliente2.cpf = 44488855598;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 44488855598;
 
 const contaCorrenteEvelyn = new ContaCorrente();
-
 contaCorrenteEvelyn.agencia = 1001;
 contaCorrenteEvelyn.cliente = cliente1;
-contaCorrenteEvelyn.depositar(400);
+contaCorrenteEvelyn.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = null;
-// conta2.cliente.nome = "Alice";
-// conta2.cliente.cpf = 44488855598;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200;
 contaCorrenteEvelyn.transferir(valor, conta2);
-console.log(conta2);
-// console.log("valor: ", valor);
-// console.log(contaCorrenteEvelyn);
+console.log(cliente1);
