c5ec18765100079fc7fd214f92045eb2bc876194
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-04-20 17:45:27 -0300

    Refactor get method and constructors

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Evelyn";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Evelyn", 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 44488855598;
+const cliente2 = new Cliente("Alice", 44488855598);
+console.log(cliente2.cpf);
 
-const contaCorrenteEvelyn = new ContaCorrente();
-contaCorrenteEvelyn.agencia = 1001;
-contaCorrenteEvelyn.cliente = cliente1;
+const contaCorrenteEvelyn = new ContaCorrente(1001, cliente1);
 contaCorrenteEvelyn.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
 let valor = 200;
 contaCorrenteEvelyn.transferir(valor, conta2);
-console.log(cliente1);
+console.log(cliente2);
